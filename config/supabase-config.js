c0d43c5900913d3497cfb9c0ff73e52df9cf355f
Idan Nahum <idanahum1122@gmail.com>
2026-05-25 00:28:29 +0300

    Use legacy anon Supabase key for client access

--- config/supabase-config.js
+++ config/supabase-config.js
@@ -2,5 +2,5 @@
   "use strict";
 
   global.PORTZOT_SUPABASE_URL = "https://lmccphquxksnyvhfgsiw.supabase.co";
-  global.PORTZOT_SUPABASE_ANON_KEY = "sb_publishable_-4oLSvf6KUpAVZ9NgvPo9A_Pzra_Vwi";
+  global.PORTZOT_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtY2NwaHF1eGtzbnl2aGZnc2l3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTIxMjYsImV4cCI6MjA5NDI2ODEyNn0.UN7S6HUf0deo287LEEZDtfKZwN1DHK8lLLKhRDFUNds";
 })(window);
